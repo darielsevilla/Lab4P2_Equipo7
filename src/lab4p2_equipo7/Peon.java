b0240891ffc90f0ae6273eddee3388cb90100879
Josue Ham <josue.ham@unitec.edu>
2023-02-10 17:23:24 -0600

    Clase peon creada

---
+++ src/lab4p2_equipo7/Peon.java
@@ -0,0 +1,44 @@
+package lab4p2_equipo7;
+
+
+public class Peon extends pieza {
+
+    public Peon() {
+        super();
+    }
+
+    public Peon(int x, int y, boolean blanca) {
+        super();
+        if (blanca) {
+            this.nombre = "P";
+            this.blanco = true;
+        } else {
+            this.nombre = "p";
+            this.blanco = false;
+        }
+    }
+
+    @Override
+    public boolean movimiento(int x, int y, int nX, int nY, Object[][] tablero) {
+        boolean mover = true;
+        if (y == 1 && blanco == true) {
+            if (nY >= 2 && nY > y) {
+            } else {
+                mover = false;
+            }
+            if (y == 6 && blanco == false) {
+                if (nY <= 2 && nY < y) {
+                } else  {
+                    mover = false;
+                }
+            } 
+            if (y!=1 && blanco == true || y != 6 && blanco ==  false) {
+                if (nY <= 1) {
+                    mover = false;
+                }
+            }
+   
+        }
+        return mover;
+    }
+}
