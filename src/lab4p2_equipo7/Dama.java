b6aa3463fba3c60b3022fd1b866e2bba2de064cc
Dariel Sevilla <darielsevilla@gmail.com>
2023-02-10 17:20:54 -0600

    reina completa

--- src/lab4p2_equipo7/Dama.java
+++ src/lab4p2_equipo7/Dama.java
@@ -16,17 +16,17 @@ public class Dama extends pieza{
         super();
     }
 
-    public Dama( boolean blanca) {
+    public Dama(boolean blanca) {
          super();
         if(blanca){
-            this.nombre = "D";
-            this.x = 3;
-            this.y = 0;
-            this.blanco = true;
-        }else{
-            this.nombre = "d";
+            this.nombre = "Q";
             this.x = 3;
             this.y = 7;
+            this.blanco = true;
+        }else{
+            this.nombre = "q";
+            this.x = 3;
+            this.y = 0;
             this.blanco = true;
         }
     }
@@ -34,17 +34,17 @@ public class Dama extends pieza{
    
 
     @Override
-    public boolean movimiento(int x, int y, int nX, int nY, Object[][] tablero) {
+public boolean movimiento(int x, int y, int nX, int nY, Object[][] tablero) {
         ArrayList<int[]> pValidas = new ArrayList();
         //for para todos los movimientos a la derecha, representados por arrays
         for (int i = x+1; i < tablero[0].length; i++) {
-            if(tablero[i][y] == null){
+            if(tablero[y][i] == null){
                 int[] pos = new int[2];
                 pos[0] = i;
                 pos[1] = y;
                 pValidas.add(pos);
             }else{
-                if(((pieza)tablero[i][y]).isBlanco() != blanco){
+                if(((pieza)tablero[y][i]).isBlanco() != blanco){
                     int[] pos = new int[2];
                     pos[0] = i;
                     pos[1] = y;
@@ -54,14 +54,14 @@ public class Dama extends pieza{
             }
         }
         //for para todos los movimientos a la izquierda, representado por arrays
-        for (int i = x-1; i > 0; i--) {
-            if(tablero[i][y] == null){
+        for (int i = x-1; i >= 0; i--) {
+            if(tablero[y][i] == null){
                 int[] pos = new int[2];
                 pos[0] = i;
                 pos[1] = y;
                 pValidas.add(pos);
             }else{
-                if(((pieza)tablero[i][y]).isBlanco() != blanco){
+                if(((pieza)tablero[y][i]).isBlanco() != blanco){
                     int[] pos = new int[2];
                     pos[0] = i;
                     pos[1] = y;
@@ -71,14 +71,14 @@ public class Dama extends pieza{
             }
         }
         //for para todos los movimientos arriba, representado por arrays
-        for (int i = y-1; i > 0; i++) {
-            if(tablero[x][i] == null){
+        for (int i = y-1; i >= 0; i--) {
+            if(tablero[i][x] == null){
                 int[] pos = new int[2];
                 pos[0] = x;
                 pos[1] = i;
                 pValidas.add(pos);
             }else{
-                if(((pieza)tablero[x][i]).isBlanco() != blanco){
+                if(((pieza)tablero[i][x]).isBlanco() != blanco){
                     int[] pos = new int[2];
                     pos[0] = x;
                     pos[1] = i;
@@ -89,13 +89,13 @@ public class Dama extends pieza{
         }
         //for para todos los movimientos abajo, representado por arrays
         for (int i = y+1; i < tablero.length || i > tablero[0].length; i++) {
-            if(tablero[x][i] == null){
+            if(tablero[i][x] == null){
                 int[] pos = new int[2];
                 pos[0] = x;
                 pos[1] = i;
                 pValidas.add(pos);
             }else{
-                if(((pieza)tablero[x][i]).isBlanco() != blanco){
+                if(((pieza)tablero[i][x]).isBlanco() != blanco){
                     int[] pos = new int[2];
                     pos[0] = x;
                     pos[1] = i;
@@ -105,23 +105,93 @@ public class Dama extends pieza{
             }
         }
         //for para los movimientos en diagonal arriba derecha, representado por arrays
-        for (int i = y-1, j = x + 1; i < tablero.length; i++) {
-            if(tablero[x][i] == null){
+        for (int i = y-1, j = x + 1; i >= 0 && j < tablero.length; i--, j++) {
+            if(tablero[i][j] == null){
                 int[] pos = new int[2];
-                pos[0] = x;
+                pos[0] = j;
                 pos[1] = i;
                 pValidas.add(pos);
             }else{
-                if(((pieza)tablero[x][i]).isBlanco() != blanco){
+                if(((pieza)tablero[i][j]).isBlanco() != blanco){
                     int[] pos = new int[2];
-                    pos[0] = x;
+                    pos[0] = j;
                     pos[1] = i;
                     pValidas.add(pos);
                 }
                 break;
             }
         }
-        return false;
+        //for para los movimientos en diagonal arriba izquierda, representado por arrays
+        for (int i = y - 1, j = x - 1; i >= 0 && j >= 0; i--, j--) {
+
+            
+            if(tablero[i][j] == null){
+                int[] pos = new int[2];
+                pos[0] = j;
+                pos[1] = i;
+                pValidas.add(pos);
+            }else{
+                if(((pieza)tablero[i][j]).isBlanco() != blanco){
+                    int[] pos = new int[2];
+                    pos[0] = j;
+                    pos[1] = i;
+                    pValidas.add(pos);
+                }
+                break;
+            }
+        }
+        //for para los movimientos en diagonal abajo izquierda, representado por arrays
+        for (int i = y + 1, j = x - 1; i < tablero.length && j >= 0; i--, j++) {
+            if(tablero[i][j] == null){
+                int[] pos = new int[2];
+                pos[0] = j;
+                pos[1] = i;
+                pValidas.add(pos);
+            }else{
+                if(((pieza)tablero[i][j]).isBlanco() != blanco){
+                    int[] pos = new int[2];
+                    pos[0] = j;
+                    pos[1] = i;
+                    pValidas.add(pos);
+                }
+                break;
+            }
+        }
+        //for para los movimientos diagonal abajo derecha, representado por arrays
+        for (int i = y + 1, j = x + 1; i < tablero.length && j < tablero.length; i++, j++) {
+           
+            if(tablero[i][j] == null){
+                int[] pos = new int[2];
+                pos[0] = j;
+                pos[1] = i;
+                pValidas.add(pos);
+            }else{
+                if(((pieza)tablero[i][j]).isBlanco() != blanco){
+                    int[] pos = new int[2];
+                    pos[0] = j;
+                    pos[1] = i;
+                    pValidas.add(pos);
+                }
+                break;
+            }
+        }
+        
+        //boolean para validar
+        int[] validar = new int[2];
+        validar[0] = nX;
+        validar[1] = nY;
+        
+       
+        for (int[] i : pValidas) {
+            
+            if(i[0] == validar[0] && i[1] == validar[1]){
+                return true;
+            }
+        }
+        
+       return false;
+        
+        
         
     }
     
--- src/lab4p2_equipo7/Lab4P2_Equipo7.java
+++ src/lab4p2_equipo7/Lab4P2_Equipo7.java
@@ -13,7 +13,84 @@ public class Lab4P2_Equipo7 {
 
     public static void main(String[] args) {
         Object[][] tablero = new Object[8][8];
+        Dama Dblanca = new Dama(true);
+        tablero[Dblanca.getY()][Dblanca.getX()] = Dblanca;
         imprimir(tablero);
+        
+        System.out.println("Jugador blancar: ");
+        String j1 = leer.nextLine();
+        System.out.println("Jugador negro: ");
+        String j2 = leer.nextLine();
+        String input = "";
+        int turno = 0;
+        do{
+            if(turno == 0){
+                System.out.println("Juega " + j1 + ":");
+                
+            }else{
+                System.out.println("Juega " + j2 + ":");
+            }
+            //pedir entrada
+            input = leer.next();
+                if(input.equals("gusbai")){
+                    break;
+                }
+                //validacion
+                if(input.contains("|") && input.contains("-")){
+                    String[] split = input.split("\\|");
+                    String[] split2 = split[1].split("-");
+                    for (String a : split) {
+                        System.out.println(a);
+                    }
+                    System.out.println(tablero[7][3]);
+                    
+                    String nombre = split[0];
+                    String posInicial = split2[0].toUpperCase();
+                    String posNueva = split2[1].toUpperCase();
+                    System.out.println(nombre);
+                    int[] posIn = new int[2];
+                    int[] posFin = new int[2];
+                    //columna
+                    posIn[0] = ((int) posInicial.charAt(0)) - 65 ;
+                    //fila
+                    posIn[1] =  Integer.parseInt(Character.toString(posInicial.charAt(1))) - 1;
+                   
+                    posFin[0] = ((int) posNueva.charAt(0)) - 65 ;
+                    posFin[1] =  Integer.parseInt(Character.toString(posNueva.charAt(1))) - 1;
+                   
+                    if(posIn[0] <= 8 && posIn[0] >= 0 && posIn[1] <= 8 && posIn[1] >= 0){
+                        if(tablero[posIn[1]][posIn[0]] != null){
+                            if(((pieza)tablero[posIn[1]][posIn[0]]).getNombre().equals(nombre)){
+                                if(turno == 0 && ((pieza)tablero[posIn[1]][posIn[0]]).isBlanco() || turno == 1 && !((pieza)tablero[posIn[0]][posIn[1]]).isBlanco()){
+                                        
+                                    System.out.println(posFin[1]);
+                                    System.out.println(posFin[0]);
+                                    if(((pieza)tablero[posIn[1]][posIn[0]]).movimiento(posIn[0], posIn[1], posFin[0], posFin[1], tablero)){
+                                        tablero[posFin[1]][posFin[0]] = ((pieza)tablero[posIn[1]][posIn[0]]);
+                                        tablero[posIn[1]][posIn[0]] = null;
+                                    }else{
+                                        System.out.println("Movimiento no valido");
+                                    }
+                                }
+                            }else{
+                                System.out.println("Nombre invalido");
+                            }
+                        }else{
+                            System.out.println("Alli no hay una pueza");
+                        }
+                    }
+                }else{
+                    System.out.println("Ingrese notacion valida");
+                }
+                imprimir(tablero);
+                if(turno == 0){
+                    turno++;
+                }else{
+                    turno--;
+                }
+        }while(!input.equals("gusbai"));
+        
+        
 
     }//Fin del main 
 
@@ -31,7 +108,16 @@ public class Lab4P2_Equipo7 {
             }
             System.out.println("");
         }
-
+        
+        
     }//Fin metodo imprimir
 
+    public static boolean checkInt(String par){
+        try{
+            Integer.parseInt(par);
+            return true;
+        }catch(Exception e){
+            return false;
+        }
+    }
 }//Fin de la clase
